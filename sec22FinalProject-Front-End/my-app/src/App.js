418d977c84a5260be2dad4ea2b80b15e8261ddf3
arduino731 <arduino731@gmail.com>
2018-07-31 22:42:11 -0500

    sec22

---
+++ sec22FinalProject-Front-End/my-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import Particles from 'react-particles-js';
+// import Clarifai from 'clarifai';
+// import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+// import Signin from './components/Signin/Signin';
+// import Register from './components/Register/Register';
+// import Logo from './components/Logo/Logo';
+// import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+// import Rank from './components/Rank/Rank';
+
+
+
+class App extends Component {
+    render(){
+        return (
+            <div className="App">
+                <Navigation />
+                {/* <Logo /> */}
+                {/* <ImageLinkForm /> */}
+                {/* <FaceRecognition />  */}
+
+            </div>
+
+        )
+    }
+}
+
+export default App;
