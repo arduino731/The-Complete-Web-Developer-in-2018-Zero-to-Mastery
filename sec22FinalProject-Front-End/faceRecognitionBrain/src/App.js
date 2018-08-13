c715d7da7265ae82133cc788786fe40f8f83713e
arduino731 <arduino731@gmail.com>
2018-08-13 17:31:59 -0500

    updated

--- sec22FinalProject-Front-End/faceRecognitionBrain/src/App.js
+++ sec22FinalProject-Front-End/faceRecognitionBrain/src/App.js
@@ -4,11 +4,15 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-// import Clarifai from 'clarifai';
+import Clarifai from 'clarifai';
 // import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 // import Signin from './components/Signin/Signin';
 // import Register from './components/Register/Register';
 import './App.css';
+
+const app = new Clarifai.App({
+       apiKey: 'be7fd62d45024a9f830c421aed281f2c'
+      });
 
 const particlesOptions = {
     particles: {
@@ -18,36 +22,49 @@ const particlesOptions = {
                 enable: true,
                 value_area: 800
             }
-        },
-        interactivity: {
-            detect_on: "canvas",
-            events: {
-              onhover: {
-                enable: false,
-                mode: "repulse"
-              },
-              onclick: {
-                enable: true,
-                mode: "push"
-              },
-              resize: true
-            }
-        },
-        modes: {
-            repulse: {
-                distance: 400,
-                duration: 0.4
-            },
-            push: {
-                particles_nb: 4
-            },
-            remove: {
-                particles_nb: 2
-            }
         }
+        // },
+        // interactivity: {
+        //     detect_on: "canvas",
+        //     events: {
+        //       onhover: {
+        //         enable: false,
+        //         mode: "repulse"
+        //       },
+        //       onclick: {
+        //         enable: true,
+        //         mode: "push"
+        //       },
+        //       resize: true
+        //     }
+        // },
+        // modes: {
+        //     repulse: {
+        //         distance: 400,
+        //         duration: 0.4
+        //     },
+        //     push: {
+        //         particles_nb: 4
+        //     },
+        //     remove: {
+        //         particles_nb: 2
+        //     }
+        // }
     }
 }
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            input: '',
+        }
+    }
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    }
+    onButtonSubmit = () => {
+        console.log('click')
+    }
     render(){
         return (
             <div className="App">
@@ -62,7 +79,10 @@ class App extends Component {
                 <Navigation />
                 <Logo />
                 <Rank />
-                <ImageLinkForm />
+                <ImageLinkForm 
+                    onInputChange = {this.onInputChange} 
+                    onButtonSubmit={this.onButtonSubmit} 
+                />
                 {/* <FaceRecognition />  */}
 
             </div>
